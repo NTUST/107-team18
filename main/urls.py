834605e1dddbbc2fccd5e07e484894a76e970fcd
SheiUn <d215039@outlook.com>
2018-05-30 14:55:42 +0800

    New

--- main/urls.py
+++ main/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # index/
     url(r'^index/$', views.index, name='index'),
-    # # signup/
-    # url(r'^signup/$', views.signup, name='signup'),
-    # # login/
-    # url(r'^login/$', views.login, name='login'),
-    # # logout/
-    # url(r'^logout/$', views.logout, name='logout'),
+    # signup/
+    url(r'^signup/$', views.signup, name='signup'),
+    # login/
+    url(r'^login/$', views.login, name='login'),
+    # logout/
+    url(r'^logout/$', views.logout, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,28 +1,35 @@
 from django.contrib import auth, messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from courses.models import CourseInformation
 
 def index(request):
     messages.info(request, '歡迎來到 Coper Files!')
+    # 精選課程 (取出檔案最多的三個課程)
+    # courses = CourseInformation.objects.values('id').annotate(files_count=Count('files')).order_by('-files_count')[:3]
     return render(request, 'main/index.html')
 
-# def signup(request):
-#     return render('main/signup.html')
+def signup(request):
+    return render('main/signup.html')
 
-# def login(request):
-#     if request.user.is_authenticated(): 
-#         return HttpResponseRedirect('/index')
+def login(request):
+    if request.user.is_authenticated(): 
+        messages.info(request, '你已登入!')
+        return render(request, 'main/index.html')
 
-#     username = request.POST.get('username', '')
-#     password = request.POST.get('password', '')
-#     print(username,password)
-#     user = auth.authenticate(username=username, password=password)
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    print(username,password)
+    user = auth.authenticate(username=username, password=password)
 
-#     if user is not None and user.is_active:
-#         auth.login(request, user)
-#         return HttpResponseRedirect('/index/')
-#     else:
-#         return render_to_response('main/login.html') 
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        messages.info(request, '登入成功!')
+        return render(request, 'main/index.html')
+    else:
+        messages.info(request, '登入失敗!')
+        return render(request, 'main/login.html') 
 
-# def logout(request):
-#     return render_to_response('main/logout.html')
+def logout(request):
+    messages.info(request, '登出成功!')
+    return render(request, 'main/logout.html')
