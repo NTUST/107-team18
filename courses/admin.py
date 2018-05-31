6184bed50fa95f950d6bdee76480c61c965d4e1f
Fong0975 <apollo0975@hotmail.com>
2018-05-31 13:22:46 +0800

    del

--- courses/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CourseAdminstrator, CourseInformation, CourseFile
-
-admin.site.register(CourseAdminstrator)
-admin.site.register(CourseInformation)
-admin.site.register(CourseFile)
