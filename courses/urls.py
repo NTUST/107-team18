1e3f0f0bef37af9eddd3c391da25c4da87a124d3
Fong0975 <apollo0975@hotmail.com>
2018-06-05 14:51:22 +0800

    0605

--- courses/urls.py
+++ courses/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # courses/
+    url(r'^$', views.courses, name='courses'),
+    # courses/<course_no>  # couser_no 為 XX123214124
+    url(r'^(?P<id>[\d]+)/$', views.courses_detail, name='courses_detail'),
     # courses/upload
     url(r'^upload/$', views.courses_files_upload, name='courses_files_upload'),
     # courses/edit
     url(r'^edit/$', views.courses_files_edit, name='courses_files_edit'),
-    # courses/
-    url(r'^$', views.courses, name='courses'),
-    # courses/<course_no>  # couser_no 為 XX123214124
-    url(r'^(?P<course_no>[\w]+)/$', views.courses_detail, name='courses_detail'),
 ]
--- courses/views.py
+++ courses/views.py
@@ -4,31 +4,31 @@ from django.shortcuts import render, get_object_or_404
 from .models import CourseAdminstrator, CourseInformation, CourseFile
 from .forms import CourseFileForm
 
-def courses(request): # Get Pages
-    # if request.method = 'POST': # Search
-    #     courses = CourseInformation.objects.filter() # Filter: Admin / Teacher / Name
-    # else: # Not Search
-    courses = CourseInformation.objects.all()
-
-    
-    # Get Top 5 By Pages
-    # Get Files length Depend on Pages 
+def courses(request):
+    if request.method == 'POST': # Search
+        courses = CourseInformation.objects.filter() # Filter: Admin / Teacher / Name
+    else: # Not Search
+        courses = CourseInformation.objects.all()
     return render(request, 'courses/index.html', {'courses': courses})
 
-def courses_detail(request, course_no):
-    # is authorize
-    # try:
-    #     course = CourseInformation.objects.get(course_no=course_no)
-    # except:
-    #     messages.info(request, '找不到課程!')
-    #     return render(request, "courses/index.html")
+def courses_detail(request, id):
+    if not request.user.is_authenticated:
+        messages.info(request, '請先登入')
+        return render(request, 'main/login.html')
 
+    try:
+        course = CourseInformation.objects.get(pk=id)
+    except:
+        messages.info(request, '找不到課程!')
+        return render(request, 'courses/index.html')
 
-    return render(request, 'courses/detail.html')
-    # return render(request, 'courses/detail.html', {'course': course})
+    return render(request, 'courses/detail.html', {'course': course})
 
 def courses_files_upload(request): # 帶著 files 進來
-    # is authorize
+    if not request.user.is_authenticated():
+        messages.info(request, '請先登入!')
+        return render(request, 'main/login.html')
+    
     if request.Post: #如果是上傳檔案
         form = CourseFileForm(request.POST)
         if form.is_valid():
@@ -38,5 +38,8 @@ def courses_files_upload(request): # 帶著 files 進來
     return HttpResponse('upload')
 
 def courses_files_edit(request):
-    # is authorize
+    if not request.user.is_authenticated():
+        messages.info(request, '請先登入!')
+        return render(request, 'main/login.html')
+
     return render(request, 'courses/edit.html')
