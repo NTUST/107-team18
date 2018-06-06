252e0d02138be0b11807436d0f558362181940ac
SheiUn <d215039@outlook.com>
2018-06-06 08:43:13 +0800

    Edit Urls of courses index.html

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,5 @@
 from django.contrib import auth, messages
+from django.db.models import Count
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import CourseAdminstrator, CourseInformation, CourseFile
@@ -6,9 +7,22 @@ from .forms import CourseFileForm
 
 def courses(request):
     if request.method == 'POST': # Search
-        courses = CourseInformation.objects.filter() # Filter: Admin / Teacher / Name
+        # 屬性名稱: course_administrator__admin_name / course_teacher / course_name
+        # 屬性名稱__icontains='搜尋條件'
+        # contains 區分大小寫 / icontains 不區分大小寫
+        mode = request.POST.get('mode')
+        codintion = request.POST.get('condition')
+        if mode == 'admin':
+            courses = CourseInformation.objects.filter(course_administrator__admin_name__icontains=codintion).annotate(file_count=Count('coursefile')).order_by('-file_count')
+        elif mode == 'teacher':
+            courses = CourseInformation.objects.filter(course_teacher__icontains=codintion).annotate(file_count=Count('coursefile')).order_by('-file_count')
+        elif mode == 'name':
+            courses = CourseInformation.objects.filter(course_name__icontains=codintion).annotate(file_count=Count('coursefile')).order_by('-file_count')
+        else:
+            messages.info(request, '搜尋錯誤!')
+            courses = CourseInformation.objects.annotate(file_count=Count('coursefile')).order_by('-file_count')
     else: # Not Search
-        courses = CourseInformation.objects.all()
+        courses = CourseInformation.objects.annotate(file_count=Count('coursefile')).order_by('-file_count')
     return render(request, 'courses/index.html', {'courses': courses})
 
 def courses_detail(request, id):
@@ -17,7 +31,7 @@ def courses_detail(request, id):
         return render(request, 'main/login.html')
 
     try:
-        course = CourseInformation.objects.get(pk=id)
+        course = CourseInformation.objects.filter(pk=id)
     except:
         messages.info(request, '找不到課程!')
         return render(request, 'courses/index.html')
@@ -28,7 +42,7 @@ def courses_files_upload(request): # 帶著 files 進來
     if not request.user.is_authenticated():
         messages.info(request, '請先登入!')
         return render(request, 'main/login.html')
-    
+
     if request.Post: #如果是上傳檔案
         form = CourseFileForm(request.POST)
         if form.is_valid():
